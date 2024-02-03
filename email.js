7d972f9ca6fd876778f97bc435d62758acd4338f
prabinnnn <prabinbhandari120@gmail.com>
2024-02-03 18:46:45 +0545

    add email

--- email.js
+++ email.js
@@ -1,5 +1,5 @@
 const emailvalidation = (string) => {
-  str = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,3}$/g;
+  str = /^[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Z|a-z]{2,3}$/g;
   return str.test(string);
 };
 const newemail = emailvalidation("prabinbhandari402@gmail.com");
